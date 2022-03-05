6ca2fecb9776a9577c55260914255e40f4d7a427
BodiMastiff <BodiRD@yandex.ru>
2022-03-05 14:50:45 +0300

    f
    f

---
+++ src/main/java/bodi/core.java
@@ -0,0 +1,18 @@
+package bodi;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("test");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
